f2743e08425bf0ab91345b10110dc20b9029ab48
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-14 13:19:17 -0700

    Finished palindromeChecker function

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,7 @@
 def palindromeChecker(string):
-	return True
-
+	if string == (string[::-1]):
+		return True
+	else:
+		return False
 
 print('\nThis program will determine whether a users string is a palindrome or not')
